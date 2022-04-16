61d380a0ff5c18dc513fbd93b00f03ed26b78cc4
douglasliralima <douglasliralima@gmail.com>
2022-04-16 12:23:43 -0300

    initial version of first page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,12 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { useState } from 'react'
 
 const Home: NextPage = () => {
+  const [user, setUser] = useState("");
+  const [adultAge, setAdultAge] = useState(false);
+  console.log(user)
   return (
     <div className={styles.container}>
       <Head>
@@ -12,8 +16,40 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1>Hello World!</h1>
+
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        console.log(user);
+        console.log(adultAge);
+      }}>
+
+        <p className={styles.text}>Please, enter your full name below</p>
+        <p className={styles.text}>Only alphabetical characters are accepted</p>
+
+        <input className={styles.input}
+          type="text"
+          value={user}
+          onChange={(e) => setUser(e.target.value)}
+          placeholder="Full Name"
+        />
+
+        <label className={styles.text}>
+          <input
+            className={styles.checkbox}
+            name="age"
+            type="checkbox"
+            checked={adultAge}
+            onChange={(e) => setAdultAge(e.target.checked)} />
+          Are you older than 18 years old?
+        </label>
+
+        <button className={styles.submitButton}>
+          Enter
+        </button>
+
+      </form>
       </main>
+      
     </div>
   )
 }
