167515f71ccdfc5de8302bca37c32599fea45aa2
douglasliralima <douglasliralima@gmail.com>
2022-04-16 23:19:37 -0300

    Header

--- pages/beverages/index.tsx
+++ pages/beverages/index.tsx
@@ -1,7 +1,10 @@
 import { useObserver } from 'mobx-react';
 import type { NextPage } from 'next';
 
+import Link from 'next/link';
+import Image from 'next/image'
 
+import styles from "../../styles/Beverages.module.css";
 
 
 import { getUserstore } from '../../store/user';
@@ -9,7 +12,38 @@ import { getUserstore } from '../../store/user';
 const Beverages: NextPage = () => {
     const store = getUserstore();
     return useObserver(() => (
-        <h1>Hello world!!</h1>
+        <div>
+            <header className={styles.beverageHeader}>
+
+                <div className={`${styles.headerReturn} ${styles.inlineDiv}`}>
+                    <Link href="/">
+                        <a>
+                            <div className={styles.imageVector}> 
+                                <Image
+                                    src="/beverages/vector.png"
+                                    width={38.4}
+                                    height={38.4}
+                                    alt="Go back icon"
+                                />
+                            </div>
+                            <span className={styles.headerText} >
+                                Go back
+                            </span>
+                        </a>
+                    </Link>
+                </div>
+
+                <div className={`${styles.headerName} ${styles.inlineDiv}`}>
+                    <span className={styles.headerText}>
+                        {store.name}
+                    </span>
+                </div>
+            </header>
+
+            <main className={styles.beverageMain}>
+                <span>Hello World Card</span>
+            </main>
+        </div>
     ));
 }
 
