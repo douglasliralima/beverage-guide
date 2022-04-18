bbd26315632cc2f7274eabea7c8fa4163be5d5fc
douglasliralima <douglasliralima@gmail.com>
2022-04-18 00:08:37 -0300

    validate if the user entered only valid characters

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,20 +2,24 @@ import type { NextPage } from 'next'
 import { useRouter } from 'next/router'
 import Image from 'next/image'
 
-import { FormEvent } from 'react'
+import { FormEvent, useState } from 'react'
 import { Observer } from 'mobx-react'
 
 import { getUserstore } from '../store/user'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
+
   const store = getUserstore();
   const router = useRouter()
-
+  const [nameError, setNameError] = useState<string | undefined>(undefined);
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    if (store.name && store.legalAge) {
+    if (!store.name.match(/^[A-Za-z\s]+$/)) {
+      setNameError("Please, enter a valid name");
+    }
+    else if (store.name && store.legalAge) {
       router.push("/beverages");
     }
   }
@@ -32,11 +36,14 @@ const Home: NextPage = () => {
               <p className={styles.text}>Only alphabetical characters are accepted</p>
 
               <input className={styles.input}
+                style={nameError ? {border: "1px solid #d23131"} : {border: "1px solid #D4D4D8"}}
                 type="text"
                 value={store.name}
                 onChange={(e) => store.setName(e.target.value)}
                 placeholder="Full Name"
               />
+
+              {nameError && <p className={styles.errorText}>{nameError}</p>}
             </div>
 
             <div className={styles.formBottom}>
