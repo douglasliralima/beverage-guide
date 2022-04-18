2afb7274d97f6b6450e083d8d52b3ff6b0be4caf
douglasliralima <douglasliralima@gmail.com>
2022-04-17 23:41:19 -0300

    Review style of the first screen

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next'
 import { useRouter } from 'next/router'
+import Image from 'next/image'
 
 import { FormEvent } from 'react'
 import { Observer } from 'mobx-react'
@@ -26,36 +27,51 @@ const Home: NextPage = () => {
 
           <form onSubmit={handleSubmit}>
 
-            <p className={styles.text}>Please, enter your full name below</p>
-            <p className={styles.text}>Only alphabetical characters are accepted</p>
+            <div className={styles.formTop}>
+              <p className={styles.text}>Please, enter your full name below</p>
+              <p className={styles.text}>Only alphabetical characters are accepted</p>
 
-            <input className={styles.input}
-              type="text"
-              value={store.name}
-              onChange={(e) => store.setName(e.target.value)}
-              placeholder="Full Name"
-            />
+              <input className={styles.input}
+                type="text"
+                value={store.name}
+                onChange={(e) => store.setName(e.target.value)}
+                placeholder="Full Name"
+              />
+            </div>
 
-            <label className={styles.text}>
-              <input
-                className={styles.checkbox}
-                name="age"
-                type="checkbox"
-                checked={store.legalAge}
-                onChange={(e) => store.setLegalAge(e.target.checked)} />
-              Are you older than 18 years old?
-            </label>
+            <div className={styles.formBottom}>
 
-            <input type="submit" value="Enter" className={styles.text + " " + styles.submitButton}
-              style={store.name && store.legalAge ? {
-                background: '#5D5FEF'
-              } : {
-                cursor: "not-allowed"
-              }} />
+              <div style={{ display: "block" }}>
+                <input
+                  className={styles.checkbox}
+                  name="age"
+                  type="checkbox"
+                  checked={store.legalAge}
+                  onChange={(e) => store.setLegalAge(e.target.checked)} />
+                <span className={styles.text}>
+                  Are you older than 18 years old?
+                </span>
+              </div>
 
-
+              <button type="submit" className={styles.text + " " + styles.submitButton}
+                style={store.name && store.legalAge ? {
+                  background: '#5D5FEF'
+                } : {
+                  cursor: "not-allowed"
+                }} >
+                Enter
+              </button>
+            </div>
           </form>
         </main>
+        <footer className={styles.footer}>
+          <Image
+            src="/bee.png"
+            width={278}
+            height={278}
+            alt="Bee image"
+          />
+        </footer>
 
       </div>
     )}
